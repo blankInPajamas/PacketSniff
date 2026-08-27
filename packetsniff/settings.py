019737099d000037680be864440306c0462c08c2
blankInPajamas <ahmedishmam59@gmail.com>
2026-08-28 02:26:11 +0600

    fix: add daphne, channels and analyzer to apps

--- packetsniff/settings.py
+++ packetsniff/settings.py
@@ -31,10 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 EXTERNAL_APPS = [
-    'analyzer'
+    'channels',
+    'analyzer',
 ]
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
